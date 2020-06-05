e36775de3d0dfcd6a14418e9e43730c6e5fecc33
hoyeonsungcfal <gustjd68@likelion.org>
2020-06-06 00:15:19 +0900

    bin

---
+++ src/NextFit.java
@@ -0,0 +1,19 @@
+import java.util.ArrayList;
+
+public class NextFit implements Fit{
+    @Override
+    public void fit(ArrayList<Bin> arr, Item item) {
+        for (int i=0; i<arr.size(); i++) {
+            Bin bin = arr.get(i);
+            if(bin.check(item)) {
+                bin.update(item);
+                return;
+            }
+        }
+
+        Bin b = new Bin();
+        b.update(item);
+        arr.add(b);
+
+    }
+}
